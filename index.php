b9723826efb705a87682e2b1b843240b5cb5b814
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-09-29 23:03:01 +0600

    index.php fix

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <h1>My Blog</h1>
 
 <?php
-$result = $conn->query("SELECT * FROM posts ORDER BY created_at DESC")
+$result = $conn->query("SELECT * FROM posts ORDER BY created_at DESC");
 while($row = $result->fetch_assoc()) {
     echo "<h2><a href='post.php?id=" . $row['id'] . "'>" . $row['title'] . "</a><h2>";
     echo "<p>" . substr($row['content'], 0, 200) . "...</p>";
