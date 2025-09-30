d4dba0fe529ff11ba328ba70be064e596347e868
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-10-01 01:33:40 +0600

    added footer.php

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p style="text-aligh:center; padding:20px; background:#3949ab; color:white;">
+        &copy; <?php echo date("Y"); ?> My Blog. All rights reserved.
+</p>
+</footer>
+</body>
+</html>
