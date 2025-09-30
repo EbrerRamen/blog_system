f333180675792828130045fa4b25e3c70efaf050
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-10-01 01:52:59 +0600

    fixed typo is footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p style="text-aligh:center; padding:20px; background:#3949ab; color:white;">
+    <p style="text-align:center; padding:20px; background:#3949ab; color:white;">
         &copy; <?php echo date("Y"); ?> My Blog. All rights reserved.
 </p>
 </footer>
